09743494515e15aaa6c922c3a9954b6b4383b3dc
eg9plant <elmox89@hotmail.com>
2020-09-13 00:11:07 +0300

    More animation

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -1,21 +1,24 @@
 extends KinematicBody2D
 class_name Actor
 
-export var MAX_JUMP_TIME = 0.35
-export var FALL_FACTOR = 2
-export var LOW_JUMP_FACTOR = 20
-export var MOVEMENT_SPEED = 500
-export var GRAVITY = 9.81
+export var MAX_JUMP_TIME = 1
+export var FALL_FACTOR = 1
+export var LOW_JUMP_FACTOR = 1
+export var MOVEMENT_SPEED = 300
+export var GRAVITY = 30
 export var max_jumps = 1
 export var hit_power = 900
-export (float, 0, 1.0) var friction = 0.3
+export (float, 0, 1.0) var friction = 0.25
 export (float, 0, 1.0) var acceleration = 0.25
+
+
 
 var motion = Vector2()
 var jump_unlocked = false
 var ball_in_range = false
 var can_hit = true
 onready var ball = get_parent().get_node("Ball")
+onready var animation_player = get_node("Sprite/AnimationPlayer")
 
 export var current_direction = 0
 
@@ -27,26 +30,31 @@ export var run_key = "" # Assigned in editor
 
 func _physics_process(_delta: float) -> void:
 	get_input()
-	motion.y += 9.81
-	motion = move_and_slide(motion, Vector2(0, -1))	
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			motion.y = -600
+	jump()
+	motion.y += GRAVITY
+	motion = move_and_slide(motion, Vector2(0, -1))
+
+
 	
 func jump():
 	if motion.y > 0:
 		motion += Vector2.UP * -GRAVITY * FALL_FACTOR
-	elif motion.y < 0 and Input.is_action_just_released("jump"):
+	elif motion.y < 0 and !Input.is_action_pressed("jump"):
 		motion += Vector2.UP * -GRAVITY * LOW_JUMP_FACTOR
+		
+	if is_on_floor():
+		animation_player.play("PlayerIdle")
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -800
+			animation_player.play("PlayerJump")
 
 func get_input():
 	current_direction = 0
 	if Input.is_action_pressed(left_key):
-		current_direction -= 1
+		current_direction = -1
 	
 	if Input.is_action_pressed(right_key):
-		current_direction += 1
+		current_direction = 1
 		
 	if current_direction != 0:
 		motion.x = lerp(motion.x, current_direction * MOVEMENT_SPEED, acceleration)
