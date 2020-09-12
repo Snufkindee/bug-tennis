11b2eca7aaba20c5fb359d817efcd8210c7ce8db
Kami <kaminasri@gmail.com>
2020-09-12 22:56:18 +0300

    Add new movement and jumping

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -2,71 +2,57 @@ extends KinematicBody2D
 class_name Actor
 
 export var MAX_JUMP_TIME = 0.35
-
-export var velocity = Vector2(0,0)
-export var gravity = 4400
+export var FALL_FACTOR = 2
+export var LOW_JUMP_FACTOR = 20
+export var MOVEMENT_SPEED = 500
+export var GRAVITY = 9.81
 export var max_jumps = 1
-export var jump_power = Vector2(450, -500)
 export var hit_power = 900
+export (float, 0, 1.0) var friction = 0.3
+export (float, 0, 1.0) var acceleration = 0.25
 
+var motion = Vector2()
 var jump_unlocked = false
 var ball_in_range = false
-var collision_info
 var can_hit = true
-var curr_jump_time = 0
 onready var ball = get_parent().get_node("Ball")
 
-enum DIRECTION {
-	right = 1,
-	left = -1
-}
+export var current_direction = 0
 
-export var current_direction = DIRECTION.right
 export var jump_key = "" # Assigned in editor
+export var left_key = "" # Assigned in editor
+export var right_key = "" # Assigned in editor
+export var hit_key = "" # Assigned in editor
+export var run_key = "" # Assigned in editor
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	collision_info = move_and_collide(velocity * delta)
+func _physics_process(_delta: float) -> void:
+	get_input()
+	motion.y += 9.81
+	motion = move_and_slide(motion, Vector2(0, -1))	
 	
-	check_collision()
-	check_jump_str(delta)
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -600
+	
+func jump():
+	if motion.y > 0:
+		motion += Vector2.UP * -GRAVITY * FALL_FACTOR
+	elif motion.y < 0 and Input.is_action_just_released("jump"):
+		motion += Vector2.UP * -GRAVITY * LOW_JUMP_FACTOR
 
-func _input(event):
-	if jump_unlocked and event.is_action_pressed(jump_key):
-		jump_unlocked = false
+func get_input():
+	current_direction = 0
+	if Input.is_action_pressed(left_key):
+		current_direction -= 1
+	
+	if Input.is_action_pressed(right_key):
+		current_direction += 1
 		
-	if ball_in_range and event.is_action_pressed(jump_key):
-		ball.velocity = Vector2(ball.transform.origin - transform.origin).normalized() * hit_power
-		ball_in_range = false
-
-
-func check_collision() -> void:
-	# Collide with ground
-	if collision_info and collision_info.normal.y == -1:
-		velocity = Vector2(0,0)
-		jump_unlocked = true
-		
-	if collision_info and collision_info.normal.y == 1:
-		velocity.y = 0
-		
-	# Collide with wall
-	if collision_info and collision_info.normal.x != 0:
-		velocity.x *= -1
-		
-		if current_direction == DIRECTION.right:
-			current_direction = DIRECTION.left
-		else:
-			current_direction = DIRECTION.right
-			
-		
-
-func check_jump_str(delta: float) -> void:
-	if jump_unlocked and Input.is_action_pressed(jump_key) and curr_jump_time <= MAX_JUMP_TIME:
-		curr_jump_time += delta
-		velocity = jump_power * Vector2(current_direction, 1)
-		
-	if Input.is_action_just_released(jump_key):
-		curr_jump_time = 0
+	if current_direction != 0:
+		motion.x = lerp(motion.x, current_direction * MOVEMENT_SPEED, acceleration)
+	else:
+		motion.x = lerp(motion.x, 0, friction)
+	
 
 func _on_Range_body_entered(body: Node2D) -> void:
 	if body.name == "Ball":
